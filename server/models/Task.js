d9719619185359a3d7ab5d091e381eadb31fd9de
AddysonH <iamaddyson@gmail.com>
2020-09-21 15:17:19 -0600

    We have empty arrays

---
+++ server/models/Task.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+let Schema = mongoose.Schema
+let ObjectId = Schema.Types.ObjectId
+
+const Task = new Schema({
+    description: { type: String, required: true },
+    creatorEmail: { type: String, required: true },
+
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+Task.virtual("creator",
+    {
+        localField: "creatorEmail",
+        ref: "Profile",
+        foreignField: "email",
+        justOne: true
+    })
+export default Task
