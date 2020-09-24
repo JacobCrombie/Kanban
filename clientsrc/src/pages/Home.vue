5990a6e62d6175999a6bffa001d3d0b1c7764d36
AddysonH <iamaddyson@gmail.com>
2020-09-24 13:49:08 -0600

    fun transparent stuff

--- clientsrc/src/pages/Home.vue
+++ clientsrc/src/pages/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="home container-fluid bg-img">
-    <div class="row align-items-center">
-      <div class="col">
-        <h1 class>KanBan</h1>
+    <div>
+      <div class="row align-items-center bg-dark p-5">
+        <div class="col-12">
+          <h1 class="whiteText">KanBan</h1>
+        </div>
       </div>
     </div>
   </div>
@@ -27,10 +29,19 @@ export default {
   min-height: 100vh;
   display: flex;
   justify-content: center;
+  flex-direction: column;
 }
 .bg-img {
   background-image: url("../assets/deskimg.jpg");
   background-size: auto;
   background-repeat: no-repeat;
+}
+
+.bg-dark {
+  opacity: 0.4;
+}
+.whiteText {
+  color: white;
+  font-size: 80px;
 }
 </style>
